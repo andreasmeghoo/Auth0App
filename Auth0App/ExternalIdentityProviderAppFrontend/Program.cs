dba0e6396710e87b2a3de831e5b088e8aead4a2b
Andreas Meghoo <andyjmeghoo@gmail.com>
2025-07-19 14:34:01 -0500

    Build Frontend Login / Sign up Interface to API

--- Auth0App/ExternalIdentityProviderAppFrontend/Program.cs
+++ Auth0App/ExternalIdentityProviderAppFrontend/Program.cs
@@ -1,10 +1,68 @@
 using ExternalIdentityProviderAppFrontend.Components;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = "Auth0";
+})
+    .AddCookie()
+    .AddOpenIdConnect("Auth0", options =>
+    {
+        options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+        options.ClientId = Environment.GetEnvironmentVariable("ExternalIdentityProviderAppClientID") ?? throw new Exception("ClientID not found");
+        options.ClientSecret = Environment.GetEnvironmentVariable("ExternalIdentityProviderAppClientSecret") ?? throw new Exception ("ClientSecret not found");
+        options.ResponseType = OpenIdConnectResponseType.Code;
+
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("email");
+
+        options.CallbackPath = new PathString("/signin-auth0");
+        
+        options.ClaimsIssuer = "Auth0";
+
+        options.SaveTokens = true;
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = "name",
+            RoleClaimType = "role"
+        };
+
+        options.Events = new OpenIdConnectEvents
+        {
+            OnRedirectToIdentityProviderForSignOut = context =>
+            {
+                var logoutUri = $"https://{builder.Configuration["Auth0:Domain"]}/v2/logout?client_id={options.ClientId}";
+
+                var postLogoutUri = builder.Configuration["Auth0:LogoutReturnUrl"];
+                if (!string.IsNullOrEmpty(postLogoutUri))
+                {
+                    logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
+                }
+
+                context.Response.Redirect(logoutUri);
+                context.HandleResponse();
+
+                return Task.CompletedTask;
+            }
+        };
+    });
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddCascadingAuthenticationState();
 
 var app = builder.Build();
 
@@ -17,7 +75,8 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseAntiforgery();
 
@@ -25,4 +84,33 @@ app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
+app.MapGet("/login", async context =>
+{
+    var returnUrl = context.Request.Query["returnUrl"].FirstOrDefault() ?? "/";
+    await context.ChallengeAsync("Auth0", new AuthenticationProperties
+    {
+        RedirectUri = returnUrl
+    });
+});
+
+app.MapGet("/logout", async context =>
+{
+    var returnUrl = context.Request.Query["returnUrl"].FirstOrDefault() ?? "/";
+    await context.SignOutAsync("Auth0", new AuthenticationProperties
+    {
+        RedirectUri = returnUrl
+    });
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
+
+app.MapGet("/signup", async context =>
+{
+    var returnUrl = context.Request.Query["returnUrl"].FirstOrDefault() ?? "/";
+    var authProps = new AuthenticationProperties
+    {
+        RedirectUri = returnUrl
+    };
+    authProps.Items["screen_hint"] = "signup";
+    await context.ChallengeAsync("Auth0", authProps);
+});
 app.Run();
